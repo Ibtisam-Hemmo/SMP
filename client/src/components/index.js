7de192ae979b5c91835d7781d8903be2bb34008f
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-06 20:02:08 +0200

    Export clean code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,7 @@
 export { default as ProfileSide } from './profileSide/ProfileSide.jsx';
+export { default as ProfileLeft } from './profileLeft/ProfileLeft.jsx';
+export { default as ProfileCard } from './profileSide/profileCard/ProfileCard.jsx';
 export { default as PostSide } from './postSide/PostSide.jsx';
+export { default as RightSide } from './trendSide/RightSide.jsx';
+export { default as Search } from './profileSide/searchPart/Search.jsx'
+export { default as FollowersCard } from './profileSide/followersCard/FollowersCard.jsx'
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from './home/Home.jsx';
+export { default as Profile } from './profile/Profile.jsx';
+export { default as Auth } from './Auth/Auth.jsx';
