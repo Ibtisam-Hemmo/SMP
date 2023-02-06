3e60d2d60b509e49502da6232aa84a145c4ce94d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-06 20:01:22 +0200

    some fake trends text for dev

---
+++ client/src/TempData/tempTrends.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+    {
+        name: "Developers",
+        shares: 17,
+    },
+    {
+        name: "Divergent",
+        shares: 80.5,
+    },
+    {
+        name: "ChatGPT",
+        shares: 75.5,
+    },
+    {
+        name: "React.js",
+        shares: 72,
+    },
+    {
+        name: "WayToGo",
+        shares: 71.9,
+    },
+    {
+        name: "NeedTime",
+        shares: 20,
+    },
+];
