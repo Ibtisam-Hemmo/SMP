83e8b5feccf9b2199f38d0705c9cceab7e35b958
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-06 16:49:37 +0200

    Export clean code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,2 @@
-export { default as ProfileSide } from './profileSide/ProfileSide.jsx'
+export { default as ProfileSide } from './profileSide/ProfileSide.jsx';
+export { default as PostSide } from './postSide/PostSide.jsx';
