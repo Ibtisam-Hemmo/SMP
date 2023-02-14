5fdf068cfe8dff026861e40fa5d5c32b74d7e58b
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-14 20:03:15 +0200

    Add login route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { register } from '../controllers/auth/index.js';
+import { register, login } from '../controllers/auth/index.js';
 
 const authRouter = Router();
 
 authRouter.post('/api/v1/auth/register', register);
+authRouter.post('/api/v1/auth/login', login);
 
 export default authRouter;
