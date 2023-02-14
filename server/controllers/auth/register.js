6bf8ac0beee9ff760aa03ad53f11987a0dec940b
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-14 20:03:47 +0200

    Check if there is a user with same username

--- server/controllers/auth/register.js
+++ server/controllers/auth/register.js
@@ -1,12 +1,21 @@
 import { userModel } from '../../models/index.js';
+import bcryptjs from 'bcryptjs'
 
 const register = async (req, res) => {
     const { username, email, password, firstName, lastName } = req.body;
-    const newUser = userModel({ username, email, password, firstName, lastName });
 
     try {
-        await newUser.save();
-        res.status(200).json({ msg: `user ${newUser.username} is created successfully` })
+        const user = await userModel.findOne({ username: username });
+        if (user) res.status(400).json({ msg: 'There is already an account using this username' })
+        else {
+
+            const salt = await bcryptjs.genSalt(10);
+            const hashedPassword = await bcryptjs.hash(password, salt);
+            const newUser = userModel({ username, email, password: hashedPassword, firstName, lastName });
+
+            await newUser.save();
+            res.status(200).json({ msg: `user ${newUser.username} is created successfully` })
+        }
     } catch (error) {
         res.status(500).json({ msg: error.message })
     }
