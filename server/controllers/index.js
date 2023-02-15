a8be6f2ab1f90f963d94c09950d82c7d22faa724
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-15 20:08:38 +0200

    export clean coding

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,8 @@
 export { default as login } from './auth/login.js'
 export { default as register } from './auth/register.js'
+
 export { default as getUser } from './users/getUser.js'
 export { default as updateUser } from './users/updateUser.js'
+export { default as deleteUser } from './users/deleteUser.js'
+export { default as followUser } from './users/followUser.js'
+export { default as unFollowUser } from './users/unFollowUser.js'
