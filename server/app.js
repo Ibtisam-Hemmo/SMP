2d3e6b54b614899bd53faea94c296421b075bf4f
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-17 21:14:54 +0200

    use cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 import router from './routes/index.js';
 
 dotenv.config();
@@ -13,6 +15,7 @@ app.set('port', port);
 app.use([
     bodyParser.json({ limit: '30mb', extended: true }),
     bodyParser.urlencoded({ limit: '30mb', extended: true }),
+    cors()
 ]);
 app.get('/', (req, res) => {
     res.send('Hello there!')
