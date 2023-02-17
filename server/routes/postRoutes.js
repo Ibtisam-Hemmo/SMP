c9e26db61b431220215c1ff1d40ce89f09e4d622
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-17 18:37:01 +0200

    update post routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { createPost,getPost } from '../controllers/index.js';
+import { createPost, deletePost, getPost, updatePost, likePost, feed } from '../controllers/index.js';
 
 const postRouter = Router();
 
 postRouter.post('/', createPost)
 postRouter.get('/:id', getPost)
+postRouter.put('/:postId', updatePost)
+postRouter.delete('/:postId', deletePost)
+postRouter.put('/:postId/like', likePost)
+postRouter.get('/:id/feed', feed)
 
 export default postRouter;
