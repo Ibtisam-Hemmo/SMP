586a8d2d2527bca88ab85e9b3b62e570edeed588
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-19 11:55:34 +0200

    React routes for auth and non auth users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
+import { useSelector } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import "./App.css";
 import { Profile, Home, Auth } from "./pages";
+
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
+
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      <Auth />
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/home" /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+      </Routes>
     </div>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import store from "./store/reduxStore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<App />}
+      />
+    </Routes>
+    </BrowserRouter>
   </Provider>
 );
