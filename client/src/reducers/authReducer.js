aee8753740fbd6f6cc59cff50f31dc2bb50dcd36
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-19 21:52:46 +0200

    handling all cases

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -7,11 +7,25 @@ const authReducer = (
             return { ...state, loading: true, error: false }
 
         case "AUTH_SUCCESS":
-            localStorage.setItem("profile", JSON.stringify({...action?.data }))
+            localStorage.setItem("profile", JSON.stringify({ ...action?.data }))
             return { ...state, authData: action.data, loading: false, error: false }
 
         case "AUTH_FAIL":
             return { ...state, loading: false, error: true }
+
+        case "UPDATING_START":
+            return { ...state, updateLoading: true, error: false }
+
+        case "UPDATING_SUCCESS":
+            localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+            return { ...state, authData: action.data, updateLoading: false, error: false }
+
+        case "UPDATING_FAIL":
+            return { ...state, updateLoading: true, error: true }
+
+        case "LOG_OUT":
+            localStorage.clear()
+            return { ...state, authData: null, loading: false, error: true }
 
         default:
             return state
