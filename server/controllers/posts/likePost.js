95d8540f268f97a3b156f33a60b96577cbdb87fa
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-19 19:39:41 +0200

    fixing bug

--- server/controllers/posts/likePost.js
+++ server/controllers/posts/likePost.js
@@ -2,7 +2,7 @@ import { postModel } from '../../models/index.js';
 
 const likePost = async (req, res) => {
     const { postId } = req.params;
-    const userId = req.body;
+    const { userId } = req.body;
 
     try {
         const post = await postModel.findById(postId)
@@ -11,7 +11,7 @@ const likePost = async (req, res) => {
             res.json({ msg: "post is liked" })
         } else {
             await post.updateOne({ $pull: { likes: userId } })
-            res.json({ msg: "post is unLiked" })        
+            res.json({ msg: "post is unLiked" })
         }
     } catch (error) {
         res.status(500).json({ msg: error.message })
