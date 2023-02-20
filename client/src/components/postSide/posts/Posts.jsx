61eed3ce083995d15fa3dc18a32bd0c6dc4aa32a
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-20 18:19:41 +0200

    handling user's posts

--- client/src/components/postSide/posts/Posts.jsx
+++ client/src/components/postSide/posts/Posts.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { getFeedPosts } from "../../../actions/index.js";
 
 import Post from "../post/Post";
 import "./posts.css";
 
-const Posts = ({ location }) => {
+const Posts = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.authReducer.authData);
-  const { posts, loading } = useSelector((state) => state.postReducer);
+  let { posts, loading } = useSelector((state) => state.postReducer);
+  const params = useParams();
 
   useEffect(() => {
     dispatch(getFeedPosts(user._id));
   }, []);
+
+  if(!posts) return 'No Posts';
+  if(params.id) posts = posts.filter((post)=> post.userId === params.id)
 
   return (
     <div className="posts">
       {loading
         ? "Fetching Posts..."
         : posts?.map((post, index) => {
-            if (location === "profile page") {
-              if (post.userId === user._id) {
-                return <Post post={post} key={index} />;
-              }
-            } else {
-              return <Post post={post} key={index} />;
-            }
+            return <Post post={post} key={index} />;
           })}
     </div>
   );
