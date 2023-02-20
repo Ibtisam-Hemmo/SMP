892d0fd6c0d28bec20d70fcbddbf3b32cbe8aa1c
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-20 13:11:10 +0200

    following and getting users API

--- client/src/apis/userReduest.js
+++ client/src/apis/userReduest.js
@@ -4,3 +4,6 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const getUser = (id) => API.get(`/api/v1/user/${id}`);
 export const UpdateUser = (id, formData) => API.put(`/api/v1/user/${id}`, formData);
+export const FollowUser = (id,data)=> API.put(`/api/v1/user/${id}/follow`, data);
+export const UnFollowUser = (id,data)=> API.put(`/api/v1/user/${id}/unFollow`, data)
+export const getAllUsers = () => API.get(`/api/v1/user/`);
