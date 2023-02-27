4f781b1a1d61cb9e5afb9389fb5e673172c69933
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-27 18:33:12 +0200

    Preventing creating existing chats

--- server/controllers/chat/createChat.js
+++ server/controllers/chat/createChat.js
@@ -3,15 +3,21 @@ import { chatModel } from '../../models/index.js';
 const createChat = async (req, res) => {
     const { userId, id } = req.body;
 
-    const newChat = new chatModel({
-        members: [userId, id],
-    });
+    const chatFound = await chatModel.findOne({ members: [  userId, id ] });
+    if (chatFound) res.status(200).json(chatFound)
+    else {
 
-    try {
-        const result = await newChat.save();
-        res.json(result);
-    } catch (error) {
-        res.status(500).json(error);
+
+        const newChat = new chatModel({
+            members: [userId, id],
+        });
+
+        try {
+            const result = await newChat.save();
+            res.json(result);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 }
 
