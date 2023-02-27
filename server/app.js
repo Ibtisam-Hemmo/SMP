f1f564c4fd443a67e1397c4b70ebce4e19cc6918
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-27 18:32:02 +0200

    Serving files in production mode correctly

--- server/app.js
+++ server/app.js
@@ -26,9 +26,6 @@ app.use([
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
-  });
 }
 
 app.use(errorMiddleware)
