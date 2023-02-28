c8af6370a860756aca1120b7cf1d86bf1cd269c3
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-02-28 20:46:23 +0200

    Wrapping code with try, catch block

--- server/controllers/chat/createChat.js
+++ server/controllers/chat/createChat.js
@@ -2,22 +2,19 @@ import { chatModel } from '../../models/index.js';
 
 const createChat = async (req, res) => {
     const { userId, id } = req.body;
+    try {
+        const chatFound = await chatModel.findOne({ members: [userId, id] });
+        if (chatFound) res.json(chatFound)
+        else {
+            const newChat = new chatModel({
+                members: [userId, id],
+            });
 
-    const chatFound = await chatModel.findOne({ members: [  userId, id ] });
-    if (chatFound) res.status(200).json(chatFound)
-    else {
-
-
-        const newChat = new chatModel({
-            members: [userId, id],
-        });
-
-        try {
             const result = await newChat.save();
             res.json(result);
-        } catch (error) {
-            res.status(500).json(error);
         }
+    } catch (error) {
+        next(error);
     }
 }
 
