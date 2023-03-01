63a3b5effff74edc761b1961b71dff60206c2b09
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-01 19:55:22 +0200

    Adding all the necessary inputs for sign up process

--- client/src/pages/Auth/signup/Signup.jsx
+++ client/src/pages/Auth/signup/Signup.jsx
@@ -3,12 +3,14 @@ import { useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import swal from "sweetalert";
 
-import signup from "../../../actions/auth/signup";
+import { signUp } from "../../../actions/authActions";
 import styles from "./Signup.module.css";
 
 const Signup = () => {
   const [data, setData] = useState({
-    name: "",
+    username: "",
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
     confirmPassword: "",
@@ -27,18 +29,26 @@ const Signup = () => {
 
   const resetForm = () => {
     setData({
-      name: "",
+      username: "",
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
-      confirmPassword: "",
+      confirmPass: "",
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!!data.name && !!data.email && !!data.password) {
-      if (data.password === data.confirmPassword) {
-        dispatch(signup(data, navigate));
+    if (
+      !!data.username &&
+      !!data.email &&
+      !!data.password &&
+      !!data.firstName &&
+      !!data.lastName
+    ) {
+      if (data.password === data.confirmPass) {
+        dispatch(signUp(data, navigate));
       } else {
         swal("Passwords don't match");
       }
@@ -51,50 +61,79 @@ const Signup = () => {
     <div className={styles.container}>
       <form className={styles.form} onSubmit={handleSubmit}>
         <h3>Sign Up</h3>
-        <div className={styles.formGroup}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            className={styles.input}
-            value={data.name}
-            onChange={handleChange}
-            required
-          />
+        <div className={styles.group}>
+          <div className={styles.formGroup}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              className={styles.input}
+              value={data.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              className={styles.input}
+              value={data.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            name="email"
-            className={styles.input}
-            value={data.email}
-            onChange={handleChange}
-            required
-          />
+        <div className={styles.group}>
+          <div className={styles.formGroup}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              className={styles.input}
+              value={data.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className={styles.input}
+              value={data.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            className={styles.input}
-            value={data.password}
-            onChange={handleChange}
-            required
-          />
+        <div className={styles.group}>
+          <div className={styles.formGroup}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className={styles.input}
+              value={data.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPass"
+              className={styles.input}
+              value={data.confirmPass}
+              onChange={handleChange}
+              required
+            />
+          </div>
         </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            className={styles.input}
-            value={data.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
+
         <Link to="/login">
           <span
             style={{
