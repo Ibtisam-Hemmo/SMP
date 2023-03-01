5a90165a71506eab00b1f35e934810f60dad9b91
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-01 19:22:02 +0200

    Remove unnecessary imports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as Home } from './home/Home.jsx';
 export { default as Profile } from './profile/Profile.jsx';
-export { default as Auth } from './Auth/Auth.jsx';
 export { default as Chat } from './chat/Chat.jsx';
