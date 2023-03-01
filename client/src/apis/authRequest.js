815d9ba3eb97190d6e246fe2783983ffb817ee84
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-01 19:22:21 +0200

    Post logout route

--- client/src/apis/authRequest.js
+++ client/src/apis/authRequest.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const logIn = (FormData) => API.post('/api/v1/auth/login', FormData)
-export const logOut = () => API.get('/api/v1/auth/logout')
+export const logOut = () => API.post('/api/v1/auth/logout')
 export const signup = (FormData) => API.post('/api/v1/auth/register', FormData)
