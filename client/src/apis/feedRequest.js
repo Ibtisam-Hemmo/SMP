1b7733168988876bcc539d83da30cdb8c19733cb
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-02 22:36:13 +0200

    Delete Post API

--- client/src/apis/feedRequest.js
+++ client/src/apis/feedRequest.js
@@ -4,3 +4,4 @@ const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const getFeed = (id) => API.get(`/api/v1/post/${id}/feed`);
 export const likePost = (postId, userId) => API.put(`/api/v1/post/${postId}/like`, { userId: userId });
+export const deletePost = (postId, userId) => API.delete(`/api/v1/post/${postId}`, { data: { userId: userId } });
