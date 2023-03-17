5659e585fbf0af2435230b8a1288f4922fea11fa
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-17 17:27:05 +0200

    Adding protected routes

---
+++ client/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from "react-redux"
+import { Navigate, useLocation } from "react-router-dom"
+
+const ProtectedRoute = ({ children }) => {
+    const user = useSelector((state) => state.authReducer.authData);
+    let location = useLocation();
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />
+    }
+    return children
+
+};
+
+export default ProtectedRoute;
