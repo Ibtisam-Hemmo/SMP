4de85ee1941c2559b05d7c748de7f8dfdcecae5f
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-03-17 17:27:18 +0200

    Clean import & export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './home/Home.jsx';
 export { default as Profile } from './profile/Profile.jsx';
 export { default as Chat } from './chat/Chat.jsx';
+export { default as ProtectedRoute } from './ProtectedRoute.jsx';
